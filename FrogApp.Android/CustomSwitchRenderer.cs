5c34e8e7ce47ff54ec0a39f331308900bbeccff8
JJ OBrien <jj@jobrien9.com>
2020-11-27 00:15:59 -0500

    SwitchFix

---
+++ FrogApp.Android/CustomSwitchRenderer.cs
@@ -0,0 +1,64 @@
+﻿using Android.Content;
+using Android.Graphics;
+using Android.Widget;
+using FrogApp;
+using FrogApp.Droid;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+//todo: do this for ios, too
+//https://stackoverflow.com/questions/48515171/is-there-a-way-i-can-customize-a-switch-colors
+//https://stackoverflow.com/questions/48564792/switchrender-switchrender-is-obsolete-with-xamarin-forms-renderer
+[assembly: ExportRenderer(typeof(SwitchFix), typeof(CustomSwitchRenderer))]
+
+namespace FrogApp.Droid
+{
+    public class CustomSwitchRenderer : Xamarin.Forms.Platform.Android.SwitchRenderer
+    {
+        private SwitchFix view;
+
+        public CustomSwitchRenderer(Context context) : base(context)
+        {
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Xamarin.Forms.Switch> e)
+        {
+            base.OnElementChanged(e);
+            if (e.OldElement != null || e.NewElement == null)
+            {
+                return;
+            }
+
+            view = (SwitchFix)Element;
+            if (Android.OS.Build.VERSION.SdkInt >= Android.OS.BuildVersionCodes.JellyBean)
+            {
+                if (this.Control != null)
+                {
+                    if (!this.Control.Checked)
+                    {
+                        this.Control.TrackDrawable.SetColorFilter(view.SwitchOffColor.ToAndroid(), PorterDuff.Mode.SrcAtop);
+                    }
+
+                    this.Control.CheckedChange += this.OnCheckedChange;
+                }
+            }
+        }
+
+        private void OnCheckedChange(object sender, CompoundButton.CheckedChangeEventArgs e)
+        {
+            //https://stackoverflow.com/questions/49448206/xamarin-forms-switchs-toggled-event-not-being-triggered-when-using-custom-rende
+            Element.IsToggled = Control.Checked;
+
+            if (!this.Control.Checked)
+            {
+                this.Control.TrackDrawable.SetColorFilter(view.SwitchOffColor.ToAndroid(), PorterDuff.Mode.SrcAtop);
+            }
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            this.Control.CheckedChange -= this.OnCheckedChange;
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ FrogApp/SwitchFix.cs
@@ -0,0 +1,20 @@
+﻿using Xamarin.Forms;
+
+namespace FrogApp
+{
+    //this is intended to fix the invisible track when the black background is toggled
+    //based on https://stackoverflow.com/questions/48515171/is-there-a-way-i-can-customize-a-switch-colors
+    public class SwitchFix : Switch
+    {
+        public static readonly BindableProperty SwitchOffColorProperty =
+          BindableProperty.Create(nameof(SwitchOffColor),
+              typeof(Color), typeof(SwitchFix),
+              Color.Default);
+
+        public Color SwitchOffColor
+        {
+            get { return (Color)GetValue(SwitchOffColorProperty); }
+            set { SetValue(SwitchOffColorProperty, value); }
+        }
+    }
+}
